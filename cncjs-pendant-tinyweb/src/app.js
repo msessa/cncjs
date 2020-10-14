17a5c39580d611921b8ad7e85310c42f20ff0890
Matteo Sessa <matteo.sessa@gmail.com>
2020-10-14 19:00:28 +1100

    bundle pendant

---
+++ cncjs-pendant-tinyweb/src/app.js
@@ -0,0 +1,456 @@
+$(function() {
+
+var root = window;
+var cnc = root.cnc || {};
+var controller = cnc.controller;
+
+controller.on('serialport:list', function(list) {
+    var $el = $('[data-route="connection"] select[data-name="port"]');
+
+    $el.empty();
+    $.each(list, function(key, value) {
+        var $option = $('<option></option>')
+            .attr('value', value.port)
+            .attr('data-inuse', value.inuse)
+            .text(value.port);
+        $el.append($option);
+    });
+
+    if (cnc.controllerType) {
+        $('[data-route="connection"] select[data-name="controllerType"]').val(cnc.controllerType);
+    }
+    if (cnc.port) {
+        $('[data-route="connection"] select[data-name="port"]').val(cnc.port);
+    }
+    if (cnc.baudrate) {
+        $('[data-route="connection"] select[data-name="baudrate"]').val(cnc.baudrate);
+    }
+
+});
+
+controller.on('serialport:open', function(options) {
+    var controllerType = options.controllerType;
+    var port = options.port;
+    var baudrate = options.baudrate;
+
+    console.log('Connected to \'' + port + '\' at ' + baudrate + '.');
+
+    cnc.connected = true;
+    cnc.controllerType = controllerType;
+    cnc.port = port;
+    cnc.baudrate = baudrate;
+
+    $('[data-route="workspace"] [data-name="port"]').val(port);
+
+    Cookies.set('cnc.controllerType', controllerType,{ expires: 365 });
+    Cookies.set('cnc.port', port,{ expires: 365 });
+    Cookies.set('cnc.baudrate', baudrate,{ expires: 365 });
+
+    if (controllerType === 'Grbl') {
+        // Read the settings so we can determine the units for position reports
+        // This will trigger a Grbl:settings callback to set grblReportingUnits
+
+        // This has a problem: The first status report arrives before the
+        // settings report, so interpreting the numbers from the first status
+        // report is ambiguous.  Subsequent status reports are interpreted correctly.
+        // We work around that by deferring status reports until the settings report.
+        controller.writeln('$$');
+    }
+
+    root.location = '#/axes';
+});
+
+controller.on('serialport:close', function(options) {
+    var port = options.port;
+
+    console.log('Disconnected from \'' + port + '\'.');
+
+    cnc.connected = false;
+    cnc.controllerType = '';
+    cnc.port = '';
+    cnc.baudrate = 0;
+
+    $('[data-route="workspace"] [data-name="port"]').val('');
+    $('[data-route="axes"] [data-name="active-state"]').text('Not connected');
+
+    root.location = '#/connection';
+});
+
+controller.on('serialport:error', function(options) {
+    var port = options.port;
+
+    console.log('Error opening serial port \'' + port + '\'');
+
+    $('[data-route="connection"] [data-name="msg"]').html('<p style="color: red">Error opening serial port \'' + port + '\'</p>');
+
+});
+
+cnc.setXYzero = function() {
+    controller.command('gcode', 'G92 X0');
+    controller.command('gcode', 'G92 Y0');
+}
+
+cnc.setZzero = function() {
+    controller.command('gcode', 'G92 Z0');
+}
+
+cnc.displayXYdistance = function(value) {
+    let optionMap = {
+        0: 0.25,
+        1: 0.50,
+        2: 1,
+        3: 5,
+        4: 10,
+        5: 50,
+        6: 100,
+        7: 200
+    };
+    let distance = optionMap[value];
+    $('[data-route="axes"] [data-name="distanceXY"]').text(distance);
+}
+
+cnc.reConnect = function() {
+    if (cnc.controllerType && cnc.port && cnc.baudrate) {
+	controller.openPort(cnc.port, {
+            controllerType: cnc.controllerType,
+            baudrate: Number(cnc.baudrate)
+	});
+	return true;
+    }
+    return false;
+};
+
+cnc.sendMove = function(cmd) {
+    var jog = function(params) {
+        params = params || {};
+        var s = _.map(params, function(value, letter) {
+            return '' + letter + value;
+        }).join(' ');
+        controller.command('gcode', 'G91'); // relative distance
+        controller.command('gcode', 'G0 ' + s);
+        controller.command('gcode', 'G90'); // absolute distance
+    };
+    var move = function(params) {
+        params = params || {};
+        var s = _.map(params, function(value, letter) {
+            return '' + letter + value;
+        }).join(' ');
+        controller.command('gcode', 'G0 ' + s);
+    };
+    var distance = Number($('[data-route="axes"] output[data-name="distanceXY"]').val()) || 0;
+
+    var fn = {
+        'G28': function() {
+            controller.command('gcode', 'G28');
+        },
+        'G30': function() {
+            controller.command('gcode', 'G30');
+        },
+        'X0Y0Z0': function() {
+            move({ X: 0, Y: 0, Z: 0 })
+        },
+        'X0': function() {
+            move({ X: 0 });
+        },
+        'Y0': function() {
+            move({ Y: 0 });
+        },
+        'Z0': function() {
+            move({ Z: 0 });
+        },
+        'X-Y+': function() {
+            jog({ X: -distance, Y: distance });
+        },
+        'X+Y+': function() {
+            jog({ X: distance, Y: distance });
+        },
+        'X-Y-': function() {
+            jog({ X: -distance, Y: -distance });
+        },
+        'X+Y-': function() {
+            jog({ X: distance, Y: -distance });
+        },
+        'X-': function() {
+            jog({ X: -distance });
+        },
+        'X+': function() {
+            jog({ X: distance });
+        },
+        'Y-': function() {
+            jog({ Y: -distance });
+        },
+        'Y+': function() {
+            jog({ Y: distance });
+        },
+        'Z-': function() {
+            jog({ Z: -distance });
+        },
+        'Z+': function() {
+            jog({ Z: distance });
+        }
+    }[cmd];
+
+    fn && fn();
+};
+
+controller.on('serialport:read', function(data) {
+    var style = 'font-weight: bold; line-height: 20px; padding: 2px 4px; border: 1px solid; color: #222; background: #F5F5F5';
+    console.log('%cR%c', style, '', data);
+});
+
+// GRBL reports position in units according to the $13 setting,
+// independent of the GCode in/mm parser state.
+// We track the $13 value by watching for the Grbl:settings event and by
+// watching for manual changes via serialport:write.  Upon initial connection,
+// we issue a settings request in serialport:open.
+var grblReportingUnits;  // initially undefined
+
+controller.on('serialport:write', function(data) {
+    var style = 'font-weight: bold; line-height: 20px; padding: 2px 4px; border: 1px solid; color: #00529B; background: #BDE5F8';
+    console.log('%cW%c', style, '', data);
+
+    // Track manual changes to the Grbl position reporting units setting
+    // We are looking for either $13=0 or $13=1
+    if (cnc.controllerType === 'Grbl') {
+        cmd = data.split('=');
+        if (cmd.length === 2 && cmd[0] === "$13") {
+            grblReportingUnits = Number(cmd[1]) || 0;
+        }
+    }
+});
+
+// This is a copy of the Grbl:state report that came in before the Grbl:settings report
+var savedGrblState;
+
+function renderGrblState(data) {
+    var status = data.status || {};
+    var activeState = status.activeState;
+    var mpos = status.mpos;
+    var wpos = status.wpos;
+    var IDLE = 'Idle', RUN = 'Run';
+    var canClick = [IDLE, RUN].indexOf(activeState) >= 0;
+
+    var parserstate = data.parserstate || {};
+
+    // Unit conversion factor - depends on both $13 setting and parser units
+    var factor = 1.0;
+    // Number of postdecimal digits to display; 3 for in, 4 for mm
+    var digits = 4;
+
+    var mlabel = 'MPos:';
+    var wlabel = 'WPos:';
+
+    switch (parserstate.modal.units) {
+    case 'G20':
+        mlabel = 'MPos (in):';
+        wlabel = 'WPos (in):';
+        digits = 4;
+        factor = grblReportingUnits === 0 ? 1/25.4 : 1.0 ;
+        break;
+    case 'G21':
+        mlabel = 'MPos (mm):';
+        wlabel = 'WPos (mm):';
+        digits = 3;
+        factor = grblReportingUnits === 0 ? 1.0 : 25.4;
+        break;
+    }
+
+    //console.log(grblReportingUnits, factor);
+
+    mpos.x = (mpos.x * factor).toFixed(digits);
+    mpos.y = (mpos.y * factor).toFixed(digits);
+    mpos.z = (mpos.z * factor).toFixed(digits);
+
+    wpos.x = (wpos.x * factor).toFixed(digits);
+    wpos.y = (wpos.y * factor).toFixed(digits);
+    wpos.z = (wpos.z * factor).toFixed(digits);
+
+    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
+    $('[data-route="axes"] [data-name="active-state"]').text(activeState);
+    $('[data-route="axes"] [data-name="mpos-label"]').text(mlabel);
+    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
+    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
+    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
+    $('[data-route="axes"] [data-name="wpos-label"]').text(wlabel);
+    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
+    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
+    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
+}
+
+controller.on('Grbl:state', function(data) {
+    // If we do not yet know the reporting units from the $13 setting, we copy
+    // the data for later processing when we do know.
+    if (typeof grblReportingUnits === 'undefined') {
+        savedGrblState = JSON.parse(JSON.stringify(data));
+    } else {
+        renderGrblState(data);
+    }
+});
+
+controller.on('Grbl:settings', function(data) {
+    var settings = data.settings || {};
+    if (settings['$13'] !== undefined) {
+        grblReportingUnits = Number(settings['$13']) || 0;
+
+        if (typeof savedGrblState !== 'undefined') {
+            renderGrblState(savedGrblState);
+            // Don't re-render the state if we get later settings reports,
+            // as the savedGrblState is probably stale.
+            savedGrblState = undefined;
+        }
+    }
+});
+
+controller.on('Smoothie:state', function(data) {
+    var status = data.status || {};
+    var activeState = status.activeState;
+    var mpos = status.mpos;
+    var wpos = status.wpos;
+    var IDLE = 'Idle', RUN = 'Run';
+    var canClick = [IDLE, RUN].indexOf(activeState) >= 0;
+
+    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
+    $('[data-route="axes"] [data-name="active-state"]').text(activeState);
+    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
+    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
+    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
+    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
+    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
+    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
+});
+
+controller.on('TinyG:state', function(data) {
+    var sr = data.sr || {};
+    var machineState = sr.machineState;
+    var stateText = {
+        0: 'Initializing',
+        1: 'Ready',
+        2: 'Alarm',
+        3: 'Program Stop',
+        4: 'Program End',
+        5: 'Run',
+        6: 'Hold',
+        7: 'Probe',
+        8: 'Cycle',
+        9: 'Homing',
+        10: 'Jog',
+        11: 'Interlock',
+    }[machineState] || 'N/A';
+    var mpos = sr.mpos;
+    var wpos = sr.wpos;
+    var READY = 1, STOP = 3, END = 4, RUN = 5;
+    var canClick = [READY, STOP, END, RUN].indexOf(machineState) >= 0;
+    var mlabel = 'MPos:';
+    var wlabel = 'WPos:';
+    switch (sr.modal.units) {
+    case 'G20':
+        mlabel = 'MPos (in):';
+        wlabel = 'WPos (in):';
+        // TinyG reports machine coordinates in mm regardless of the in/mm mode
+        mpos.x = (mpos.x / 25.4).toFixed(4);
+        mpos.y = (mpos.y / 25.4).toFixed(4);
+        mpos.z = (mpos.z / 25.4).toFixed(4);
+        // TinyG reports work coordinates according to the in/mm mode
+        wpos.x = Number(wpos.x).toFixed(4);
+        wpos.y = Number(wpos.y).toFixed(4);
+        wpos.z = Number(wpos.z).toFixed(4);
+        break;
+    case 'G21':
+        mlabel = 'MPos (mm):';
+        wlabel = 'WPos (mm):';
+        mpos.x = Number(mpos.x).toFixed(3);
+        mpos.y = Number(mpos.y).toFixed(3);
+        mpos.z = Number(mpos.z).toFixed(3);
+        wpos.x = Number(wpos.x).toFixed(3);
+        wpos.y = Number(wpos.y).toFixed(3);
+        wpos.z = Number(wpos.z).toFixed(3);
+    }
+
+    $('[data-route="axes"] .control-pad .btn').prop('disabled', !canClick);
+    $('[data-route="axes"] [data-name="active-state"]').text(stateText);
+    $('[data-route="axes"] [data-name="mpos-label"]').text(mlabel);
+    $('[data-route="axes"] [data-name="mpos-x"]').text(mpos.x);
+    $('[data-route="axes"] [data-name="mpos-y"]').text(mpos.y);
+    $('[data-route="axes"] [data-name="mpos-z"]').text(mpos.z);
+    $('[data-route="axes"] [data-name="wpos-label"]').text(wlabel);
+    $('[data-route="axes"] [data-name="wpos-x"]').text(wpos.x);
+    $('[data-route="axes"] [data-name="wpos-y"]').text(wpos.y);
+    $('[data-route="axes"] [data-name="wpos-z"]').text(wpos.z);
+});
+
+controller.on('Marlin:state', function(data) {
+    var mlabel = 'MPos:';
+    var pos = {}
+    switch (data.modal.units) {
+        case 'G20':
+            mlabel = 'MPos (in):';
+            pos.x = (data.pos.x/25.4).toFixed(4);
+            pos.y = (data.pos.y/25.4).toFixed(4);
+            pos.z = (data.pos.z/25.4).toFixed(4);
+            break;
+        case 'G21':
+            mlabel = 'MPos (mm):';
+            pos.x = Number(data.pos.x).toFixed(3);
+            pos.y = Number(data.pos.y).toFixed(3);
+            pos.z = Number(data.pos.z).toFixed(3);
+            break;
+    }
+    // Assume if we get this callback it's because we are connected
+    var stateText = "Connected";
+
+    $('[data-route="axes"] [data-name="active-state"]').text(stateText);
+    $('[data-route="axes"] [data-name="mpos-label"]').text(mlabel);
+    $('[data-route="axes"] [data-name="mpos-x"]').text(pos.x);
+    $('[data-route="axes"] [data-name="mpos-y"]').text(pos.y);
+    $('[data-route="axes"] [data-name="mpos-z"]').text(pos.z);
+    $('[data-route="axes"] [data-name="wpos-label"]').html('<br>');
+    $('[data-route="axes"] [data-name="wpos-x"]').hide();
+    $('[data-route="axes"] [data-name="wpos-y"]').hide();
+    $('[data-route="axes"] [data-name="wpos-z"]').hide();
+});
+
+controller.on('Marlin:settings', function(data) {
+    // Sleep and unlock are not supported in Marlin
+    $('[data-route="axes"] .sleep').hide();
+    $('[data-route="axes"] .unlock').hide();
+
+    // Cyclestart and feedhold are unimplemented in Marlin so swap with gcode commands instead
+    $('[data-route="axes"] .cyclestart').attr("onclick", "cnc.controller.command('gcode:start')");
+    $('[data-route="axes"] .feedhold').attr("onclick", "cnc.controller.command('gcode:pause')");
+
+    // G30 is unsupported in Marlin, so split the G28 button into both G28 X Y and G28 Z buttons
+    $('[data-route="axes"] .g28').attr("onclick", "cnc.controller.command('gcode', 'G28 X Y')").text("G28 X Y");
+    $('[data-route="axes"] .g30').attr("onclick", "cnc.controller.command('gcode', 'G28 Z')").text("G28 Z");
+});
+
+controller.listAllPorts();
+
+// Workspace 
+$('[data-route="workspace"] [data-name="port"]').val('');
+$('[data-route="workspace"] [data-name="btn-close"]').on('click', function() {
+    controller.closePort();
+});
+
+//
+// Connection
+//
+$('[data-route="connection"] [data-name="btn-open"]').on('click', function() {
+    var controllerType = $('[data-route="connection"] [data-name="controllerType"]').val();
+    var port = $('[data-route="connection"] [data-name="port"]').val();
+    var baudrate = $('[data-route="connection"] [data-name="baudrate"]').val();
+
+    $('[data-route="connection"] [data-name="msg"]').val('');
+    controller.openPort(port, {
+        controllerType: controllerType,
+        baudrate: Number(baudrate)
+    });
+});
+
+//
+// Axes
+//
+$('[data-route="axes"] [data-name="btn-dropdown"]').dropdown();
+$('[data-route="axes"] [data-name="active-state"]').text('Not connected');
+$('[data-route="axes"] select[data-name="select-distance"]').val('1');
+
+});
---
+++ cncjs-pendant-tinyweb/src/controller.js
@@ -0,0 +1,279 @@
+$(function() {
+
+var parseParams = function(str) {
+    return str.split('&').reduce(function (params, param) {
+        if (!param) {
+            return params;
+        }
+        var paramSplit = param.split('=').map(function (value) {
+            return decodeURIComponent(value.replace('+', ' '));
+        });
+        params[paramSplit[0]] = paramSplit[1];
+        return params;
+    }, {});
+};
+
+var params = parseParams(window.location.search.slice(1));
+var root = window;
+var token = params.token || '';
+
+if (!token) {
+    try {
+        var cnc = {};
+        cnc = JSON.parse(localStorage.getItem('cnc') || {});
+        cnc.state = cnc.state || {};
+        cnc.state.session = cnc.state.session || {};
+        token = cnc.state.session.token || '';
+    } catch (err) {
+        // Ignore error
+    }
+}
+
+// WebSocket
+var socket = root.io.connect('', {
+    query: 'token=' + token
+});
+
+// Generate buttons for custom commands
+jQuery.get("../api/commands", {token: token, paging: false}, function(data) {
+    data.records.forEach(function(record) {
+        $(".actions").append( function(){
+            return $('<button type="button" class="btn" data-name="' + record.id + '">' + record.title + '</button>').on('click', function() {
+                jQuery.post("../api/commands/run/" + record.id + "?token=" + token);
+            });
+        })
+    });
+});
+
+socket.on('connect', function() {
+    $('#loading').remove(); // Remove loading message
+    root.cnc.router.init();
+    window.location = '#/';
+});
+
+socket.on('error', function() {
+    socket.destroy();
+
+    // Redirect to login page
+    window.location = '/?continue=' + encodeURIComponent(window.location.pathname);
+});
+
+socket.on('close', function() {
+});
+
+// constants
+var GRBL = 'Grbl';
+var SMOOTHIE = 'Smoothie';
+var TINYG = 'TinyG';
+var MARLIN = 'Marlin';
+
+// Workflow State
+var WORKFLOW_STATE_RUNNING = 'running';
+var WORKFLOW_STATE_PAUSED = 'paused';
+var WORKFLOW_STATE_IDLE = 'idle';
+
+var CNCController = function() {
+    this.socket = socket;
+
+    this.callbacks = {
+        //
+        // System Events
+        //
+        'startup': [],
+        'config:change': [],
+        'task:start': [],
+        'task:finish': [],
+        'task:error': [],
+        'serialport:list': [],
+        'serialport:change': [],
+        'serialport:open': [],
+        'serialport:close': [],
+        'serialport:error': [],
+        'serialport:read': [],
+        'serialport:write': [],
+        'gcode:load': [],
+        'gcode:unload': [],
+        'feeder:status': [],
+        'sender:status': [],
+        'workflow:state': [],
+        'Grbl:state': [],
+        'Grbl:settings': [],
+        'Smoothie:state': [],
+        'Smoothie:settings': [],
+        'TinyG:state': [],
+        'TinyG:settings': [],
+        'Marlin:state': [],
+        'Marlin:settings': []
+    };
+
+    this.port = '';
+    this.baudrate = 115200;
+    this.type = '';
+    this.state = {};
+    this.settings = {};
+    this.workflowState = WORKFLOW_STATE_IDLE;
+
+    Object.keys(this.callbacks).forEach(function(eventName) {
+        socket.on(eventName, function() {
+            var args = Array.prototype.slice.call(arguments);
+
+            if (eventName === 'serialport:open') {
+                this.port = args[0].port;
+                this.type = args[0].controllerType;
+            }
+            if (eventName === 'serialport:close') {
+                this.port = '';
+                this.type = '';
+                this.state = {};
+                this.settings = {};
+                this.workflowState = WORKFLOW_STATE_IDLE;
+            }
+            if (eventName === 'workflow:state') {
+                this.workflowState = args[0];
+            }
+            if (eventName === 'Grbl:state') {
+                this.type = GRBL;
+                this.state = args[0];
+            }
+            if (eventName === 'Grbl:settings') {
+                this.type = GRBL;
+                this.settings = args[0];
+            }
+            if (eventName === 'Smoothie:state') {
+                this.type = SMOOTHIE;
+                this.state = args[0];
+            }
+            if (eventName === 'Smoothie:settings') {
+                this.type = SMOOTHIE;
+                this.settings = args[0];
+            }
+            if (eventName === 'TinyG:state') {
+                this.type = TINYG;
+                this.state = args[0];
+            }
+            if (eventName === 'TinyG:settings') {
+                this.type = TINYG;
+                this.settings = args[0];
+            }
+            if (eventName === 'Marlin:state') {
+                this.type = MARLIN;
+                this.state = args[0];
+            }
+            if (eventName === 'Marlin:settings') {
+                this.type = MARLIN;
+                this.settings = args[0];
+            }
+
+            this.callbacks[eventName].forEach(function(callback) {
+                callback.apply(callback, args);
+            });
+        }.bind(this));
+    }.bind(this));
+};
+
+CNCController.prototype.on = function(eventName, callback) {
+    var callbacks = this.callbacks[eventName];
+    if (!callbacks) {
+        console.error('Undefined event name:', eventName);
+        return;
+    }
+    if (typeof callback === 'function') {
+        callbacks.push(callback);
+    }
+};
+
+CNCController.prototype.off = function(eventName, callback) {
+    var callbacks = this.callbacks[eventName];
+    if (!callbacks) {
+        console.error('Undefined event name:', eventName);
+        return;
+    }
+    if (typeof callback === 'function') {
+        callbacks.splice(callbacks.indexOf(callback), 1);
+    }
+};
+
+CNCController.prototype.openPort = function(port, options) {
+    socket.emit('open', port, options);
+
+    this.type = options.controllerType;
+    this.port = port;
+    this.baudrate = options.baudrate;
+};
+
+CNCController.prototype.closePort = function(port) {
+    port = port || this.port;
+
+    socket.emit('close', port);
+
+    this.type = '';
+    this.port = '';
+    this.baudrate = 0;
+};
+
+CNCController.prototype.listAllPorts = function() {
+    socket.emit('list');
+};
+
+// @param {string} cmd The command string
+// @example Example Usage
+// - Load G-code
+//   controller.command('gcode:load', name, gcode, callback)
+// - Unload G-code
+//   controller.command('gcode:unload')
+// - Start sending G-code
+//   controller.command('gcode:start')
+// - Stop sending G-code
+//   controller.command('gcode:stop')
+// - Pause
+//   controller.command('gcode:pause')
+// - Resume
+//   controller.command('gcode:resume')
+// - Feed Hold
+//   controller.command('feedhold')
+// - Cycle Start
+//   controller.command('cyclestart')
+// - Status Report
+//   controller.command('statusreport')
+// - Homing
+//   controller.command('homing')
+// - Sleep
+//   controller.command('sleep')
+// - Unlock
+//   controller.command('unlock')
+// - Reset
+//   controller.command('reset')
+// - Feed Override
+//   controller.command('feedOverride')
+// - Spindle Override
+//   controller.command('spindleOverride')
+// - Rapid Override
+//   controller.command('rapidOverride')
+// - G-code
+//   controller.command('gcode', 'G0X0Y0')
+// - Load a macro
+//   controller.command('macro:load', '<macro-id>', { /* optional vars */ }, callback)
+// - Run a macro
+//   controller.command('macro:run', '<macro-id>', { /* optional vars */ }, callback)
+// - Load file from a watch directory
+//   controller.command('watchdir:load', '/path/to/file', callback)
+CNCController.prototype.command = function(cmd) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    socket.emit.apply(socket, ['command', this.port, cmd].concat(args));
+};
+
+// @param {string} data The data to write.
+// @param {object} [context] The associated context information.
+CNCController.prototype.write = function(data, context) {
+    socket.emit('write', this.port, data, context);
+};
+
+// @param {string} data The data to write.
+// @param {object} [context] The associated context information.
+CNCController.prototype.writeln = function(data, context) {
+    socket.emit('writeln', this.port, data, context);
+};
+
+root.cnc.controller = new CNCController();
+
+});
